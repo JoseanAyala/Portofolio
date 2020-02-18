fbe15c14b3cb1105964b902ed2b32a9728e1f430
Josean L. Ayala-Molina <36864389+JoseanAyala@users.noreply.github.com>
2020-02-17 20:11:27 -0400

    Responsive layout

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-$( '.js-input' ).keyup(function() {
-    if( $(this).val() ) {
-       $(this).addClass('not-empty');
-    } else {
-       $(this).removeClass('not-empty');
-    }
-  });
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//    anchor.addEventListener('click', function (e) {
+//        e.preventDefault();
+
+//        document.querySelector(this.getAttribute('href')).scrollIntoView({
+//            behavior: 'smooth'
+//        });
+//    });
+// });
+$(document).on('click', 'a[href^="#"]', function (event) {
+   event.preventDefault();
+
+   $('html, body').animate({
+       scrollTop: $($.attr(this, 'href')).offset().top
+   }, 500);
+});
+
+
+
+

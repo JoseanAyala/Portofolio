9a51524e6188ffdbd12a7303d9a351b95158f535
Josean L. Ayala-Molina <36864389+JoseanAyala@users.noreply.github.com>
2020-02-24 00:55:35 -0400

    Refactor of UI

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//    anchor.addEventListener('click', function (e) {
-//        e.preventDefault();
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        center: true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false,
+                autoplay:true,
+                loop:true
+            },
+            600:{
+                items:2,
+                nav:false,
+                autoplay:false,
+                loop:false
+            },
+            1000:{
+                items:2,
+                nav:false,
+                loop:false
+            }
+        },
+        margin:10,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true
+    });
 
-//        document.querySelector(this.getAttribute('href')).scrollIntoView({
-//            behavior: 'smooth'
-//        });
-//    });
-// });
+  });
+
 $(document).on('click', 'a[href^="#"]', function (event) {
    event.preventDefault();
 
@@ -14,7 +35,6 @@ $(document).on('click', 'a[href^="#"]', function (event) {
        scrollTop: $($.attr(this, 'href')).offset().top
    }, 500);
 });
-
 
 
 

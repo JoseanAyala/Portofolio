b36c098eb442a9c8d3b2c2e110d24ae3279982e0
Josean L. Ayala-Molina <36864389+JoseanAyala@users.noreply.github.com>
2020-03-30 23:39:00 -0400

    Added scaling to clickable items.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const appTheme = {
-    'dark' : [
+    'dark': [
         '--appPrimaryBgColor:#161616',
         '--appPrimaryFontColor:#F5F5F5',
         '--appLinkColorLight:#005da7',
         '--appImageOverlayColor:rgb(0, 0, 0, 0.9)',
         '--appShadowColor:#000'
     ],
-    'light' : [
+    'light': [
         '--appPrimaryBgColor:#F5F5F5',
         '--appPrimaryFontColor:#161616',
         '--appLinkColorLight:#005da7',
@@ -15,55 +15,54 @@ const appTheme = {
     ]
 }
 
-
-  const setContentHeight = function() {
+const setContentHeight = function () {
     $('.owl-carousel .containerContentBox').attr('style', 'height:fit-content')
-    var height = 0 ;
+    var height = 0;
     var elements = document.querySelectorAll(".owl-carousel .containerContentBox")
-    for (var index = 0; index < elements.length; index++) height = height < elements[index].offsetHeight ? elements[index].offsetHeight : height;        
-    $('.owl-carousel .containerContentBox').attr('style', 'height:'+ height +'px')
-  }
+    for (var index = 0; index < elements.length; index++) height = height < elements[index].offsetHeight ? elements[index].offsetHeight : height;
+    $('.owl-carousel .containerContentBox').attr('style', 'height:' + height + 'px')
+}
 
 $(window).resize(function () {
     clearTimeout(window.resizedFinished);
     window.resizedFinished = setTimeout(setContentHeight, 250);
 })
 
-$(document).ready(function(){
+$(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         center: true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:false,
-                autoplay:true,
-                loop:true,
-                autoplayTimeout:4000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                autoplay: true,
+                loop: true,
+                autoplayTimeout: 4000,
             },
-            600:{
-                items:2,
-                nav:false,
-                autoplay:false,
-                loop:false
-                
+            600: {
+                items: 2,
+                nav: false,
+                autoplay: false,
+                loop: false
+
             },
-            1000:{
-                items:2,
-                nav:false,
-                autoplay:false,
-                loop:false
+            1000: {
+                items: 2,
+                nav: false,
+                autoplay: false,
+                loop: false
             }
         },
-        margin:10,
-        autoplayHoverPause:true
+        margin: 10,
+        autoplayHoverPause: true
     });
 
-    $('.bottomNav').on('click', 'div',  function () {
+    $('.bottomNav').on('click', 'div', function () {
         var selectedElement = $(this);
         var SelectedID = selectedElement.attr('id');
 
-        if ($('*').hasClass('fadeIn')){
+        if ($('*').hasClass('fadeIn')) {
             $('.fadeIn').removeClass('fadeIn animated');
         }
         //Remove Previous HIGHLIGHT
@@ -71,7 +70,7 @@ $(document).ready(function(){
 
         //Add HIGHLIGHT to current
         selectedElement.addClass('HIGHLIGHT');
-        
+
         //Hide previous element
         $('.DISPLAY').removeClass('DISPLAY').addClass('HDN');
 
@@ -82,20 +81,17 @@ $(document).ready(function(){
             setTimeout(setContentHeight, 300);
     });
 
-    $('.switch > input').click(function(){
-        if($(this).is(':checked')){
-            appTheme.light.map((prop)=>{
+    $('.switch > input').click(function () {
+        if ($(this).is(':checked')) {
+            appTheme.light.map((prop) => {
                 var property = prop.split(':')
                 document.documentElement.style.setProperty(property[0], property[1]);
             });
         } else {
-            appTheme.dark.map((prop)=>{
+            appTheme.dark.map((prop) => {
                 var property = prop.split(':')
                 document.documentElement.style.setProperty(property[0], property[1]);
             });
         }
     });
-
-  });
-
-
+});

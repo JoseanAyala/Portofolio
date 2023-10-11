9a8a7523a82e37967cb2bbe4a68f20c40fe4d927
Josean L. Ayala-Molina <36864389+JoseanAyala@users.noreply.github.com>
2023-10-11 03:38:48 -0400

    inital commit for vite ts tailwind portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      cream: "#F1EFEF",
+      dutchWhite: "#CCC8AA",
+      coolGrey: "#7D7C7C",
+      black: "#191717",
+    },
     extend: {},
   },
   plugins: [],

fb7cf52dff98c72b3e0c8dd9e23132a25610d47d
Josean Ayala <josean.webdev@gmail.com>
2023-10-15 02:19:05 -0400

    Forked API development to portfolio

--- api/main.go
+++ api/main.go
@@ -1,28 +1,30 @@
 package main
 
 import (
+	"api/articleUtil"
 	"fmt"
 	"log"
 	"net/http"
-
-	blog "api/blog"
 
 	"github.com/gorilla/mux"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: homePage")
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
 
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homePage).Methods("GET")
-	router.HandleFunc("/articles", blog.GetArticles).Methods("GET")
-	router.HandleFunc("/articles/{id}", blog.GetArticle).Methods("GET")
+	router.HandleFunc("/articles", articleUtil.GetArticles).Methods("GET")
+	router.HandleFunc("/articles/{id}", articleUtil.GetArticleById).Methods("GET")
+	router.HandleFunc("/articles", articleUtil.CreateArticle).Methods("POST")
+	router.HandleFunc("/articles/{id}", articleUtil.DeleteArticle).Methods("DELETE")
+	router.HandleFunc("/articles/{id}", articleUtil.UpdateArticle).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":1337", router))
 }
 
 func main() {
+	fmt.Println("Rest API")
 	handleRequests()
 }

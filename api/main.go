3c17c19dcf5708e07c8a7e886aa7b7dca1f10ffb
Josean Ayala <josean.webdev@gmail.com>
2023-10-15 02:56:43 -0400

    Update main.go

--- api/main.go
+++ api/main.go
@@ -3,29 +3,23 @@ package main
 import (
 	"api/_pkg/articleUtil"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: homePage")
 	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
 
-func handleRequests() {
+func Main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homePage).Methods("GET")
+	router.HandleFunc("/", Handler).Methods("GET")
 	router.HandleFunc("/articles", articleUtil.GetArticles).Methods("GET")
 	router.HandleFunc("/articles/{id}", articleUtil.GetArticleById).Methods("GET")
 	router.HandleFunc("/articles", articleUtil.CreateArticle).Methods("POST")
 	router.HandleFunc("/articles/{id}", articleUtil.DeleteArticle).Methods("DELETE")
 	router.HandleFunc("/articles/{id}", articleUtil.UpdateArticle).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":1337", router))
-}
-
-func Main() {
-	fmt.Println("Rest API")
-	handleRequests()
+	http.ListenAndServe(":1337", router)
 }

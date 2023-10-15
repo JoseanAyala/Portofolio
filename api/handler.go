3aedee4e5b018b344b2a7bc4355cf362d33eca0e
Josean Ayala <josean.webdev@gmail.com>
2023-10-15 03:05:13 -0400

    handler update again adn again agnaing ain

--- api/handler.go
+++ api/handler.go
@@ -9,17 +9,18 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: homePage")
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
-}
-
-func Main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Handler).Methods("GET")
+	router.HandleFunc("/", Hello).Methods("GET")
 	router.HandleFunc("/articles", articleUtil.GetArticles).Methods("GET")
 	router.HandleFunc("/articles/{id}", articleUtil.GetArticleById).Methods("GET")
 	router.HandleFunc("/articles", articleUtil.CreateArticle).Methods("POST")
 	router.HandleFunc("/articles/{id}", articleUtil.DeleteArticle).Methods("DELETE")
 	router.HandleFunc("/articles/{id}", articleUtil.UpdateArticle).Methods("PUT")
 	http.ListenAndServe(":1337", router)
+
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: homePage")
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }

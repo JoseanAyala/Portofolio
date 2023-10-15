936fc59f587814896e5e72263e01ae3d2db1431a
Josean Ayala <josean.webdev@gmail.com>
2023-10-15 02:07:25 -0400

    refactored api code

--- api/blog/blog.go
+++ api/blog/blog.go
@@ -11,34 +11,34 @@ import (
 )
 
 type article struct {
-	Id int `json:"id"`
-	Title string `json:"title"`
-	Body string `json:"body"`
-	DateCreated string `json:"dateCreated"`
+	Id           int    `json:"id"`
+	Title        string `json:"title"`
+	Body         string `json:"body"`
+	DateCreated  string `json:"dateCreated"`
 	DateModified string `json:"dateModified"`
 }
 
-func GetArticles(w http.ResponseWriter, r *http.Request){
+func GetArticles(w http.ResponseWriter, r *http.Request) {
 	articles, err := db.Query("SELECT * FROM blogPost", reflect.TypeOf(article{}))
-	if(err != nil){
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		return;
+		return
 	}
-	
+
 	fmt.Println("GetArticles Endpoint Hit")
 	json.NewEncoder(w).Encode(articles)
 }
 
-func GetArticle(w http.ResponseWriter, r *http.Request){
+func GetArticle(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 	query := "SELECT * FROM blogPost WHERE id = " + id
 	articles, err := db.Query(query, reflect.TypeOf(article{}))
-	if(err != nil){
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		return;
+		return
 	}
-	
+
 	fmt.Println("GetAllArticles Endpoint Hit")
 	json.NewEncoder(w).Encode(articles)
 }
--- api/main.go
+++ api/main.go
@@ -10,19 +10,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
+func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: homePage")
 	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
 
-func handleRequests(){
-	router:= mux.NewRouter().StrictSlash(true)
+func handleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homePage).Methods("GET")
 	router.HandleFunc("/articles", blog.GetArticles).Methods("GET")
 	router.HandleFunc("/articles/{id}", blog.GetArticle).Methods("GET")
 	log.Fatal(http.ListenAndServe(":1337", router))
 }
 
-func main(){
+func main() {
 	handleRequests()
 }

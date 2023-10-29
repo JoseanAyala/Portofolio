77034b4083ed6fc527acbde1a0c3f425f0354003
Josean Ayala <josean.webdev@gmail.com>
2023-10-29 17:09:04 -0400

    Server wake up in home page

--- src/pages/home/Homepage.tsx
+++ src/pages/home/Homepage.tsx
@@ -1,7 +1,17 @@
 import Hero from "./Hero";
 import Content from "./Content";
 import { StickyNavbar } from "src/components/StickyNavbar";
+import { useEffect } from "react";
+import useClient from "src/utils/useClient";
+
 export default function Homepage() {
+  const { get, baseUrl } = useClient();
+
+  useEffect(() => {
+    // Wake up cloud server, that is in free tier ;)
+    get(baseUrl);
+  }, []);
+
   return (
     <>
       <StickyNavbar socialMode />

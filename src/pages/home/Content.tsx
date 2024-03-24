73cec87b5a862f1c610a54ff8d7b5688b80be832
Josean L. Ayala-Molina <36864389+JoseanAyala@users.noreply.github.com>
2024-03-23 22:12:04 -0400

    removed know more section

--- src/pages/home/Content.tsx
+++ src/pages/home/Content.tsx
@@ -1,13 +1,11 @@
 import Experiences from "./sections/Experiences";
 import AboutMe from "./sections/AboutMe";
-import KnowMore from "./sections/KnowMore";
 
 export default function Content() {
   return (
     <main className="animate-fadeUp pt-12 lg:w-full lg:pb-16 lg:pt-24">
       <AboutMe />
       <Experiences />
-      <KnowMore />
     </main>
   );
 }
